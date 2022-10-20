bb1068ce3a1ac31a9da8da1465e299d30de412f3
Mateusz Kaczorowski <mmssfd0@gmail.com>
2022-10-20 19:15:20 +0200

    js comlite

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 "use strict";
 const titleClickHandler = function (event) {
   event.preventDefault();
-
-  const clickedElement = this;
-  const clickedElementActive = clickedElement.classList.contains("active");
+  ment = this;
   console.log("Link was clicked!");
 
   const activeLinks = document.querySelectorAll(".titles a.active");
+
+  if (!clickedElement.classList.contains("active")) {
+    clickedElement.classList.add("active");
+  }
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
@@ -15,18 +17,15 @@ const titleClickHandler = function (event) {
   console.log("clickedElement:", clickedElement);
 
   const activeArticles = document.querySelectorAll("article.post.active");
+  const href = clickedElement.getAttribute("href");
+  const article = document.querySelector(href);
+
+  if (!article.classList.contains("active")) {
+    article.classList.add("active");
+  }
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
-  }
-
-  const href = clickedElement.getAttribute("href");
-
-  const article = document.querySelector(href);
-
-  if (clickedElementActive == false) {
-    article.classList.add("active");
-    clickedElement.classList.add("active");
   }
 };
 
