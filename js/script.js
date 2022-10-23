ad248a90db45d09131772f3860fdf42397b74fbd
Mateusz Kaczorowski <mmssfd0@gmail.com>
2022-10-23 23:06:38 +0200

    java script finished

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 "use strict";
 const titleClickHandler = function (event) {
   event.preventDefault();
-  
+
   const clickedElement = this;
   console.log("Link was clicked!");
 
   const activeLinks = document.querySelectorAll(".titles a.active");
 
-  if (!clickedElement.classList.contains("active")) {
-    clickedElement.classList.add("active");
-  }
+  clickedElement.classList.add("active");
+  /* [DONE] add class 'active' to the clicked link */
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
+  /* [DONE] remove class 'active' from all article links  */
 
   console.log("clickedElement:", clickedElement);
 
   const activeArticles = document.querySelectorAll("article.post.active");
   const href = clickedElement.getAttribute("href");
+  /* get 'href' attribute from the clicked link */
   const article = document.querySelector(href);
+  /* find the correct article using the selector (value of 'href' attribute) */
 
-  if (!article.classList.contains("active")) {
-    article.classList.add("active");
-  }
+  article.classList.add("active");
+  /* add class 'active' to the correct article */
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
