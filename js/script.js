e077f4de35a36443a761e56cd79f858db5279357
Mateusz Kaczorowski <mmssfd0@gmail.com>
2022-10-24 12:54:45 +0200

    add generation of articles

--- js/script.js
+++ js/script.js
@@ -31,8 +31,59 @@ const titleClickHandler = function (event) {
   }
 };
 
-const links = document.querySelectorAll(".titles a");
+const optArticleSelector = ".post",
+  optTitleSelector = ".post-title",
+  optTitleListSelector = ".titles";
 
-for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
+function generateTitleLinks() {
+  const titleList = document.querySelector(optTitleListSelector);
+
+  function clearMessages() {
+    titleList.innerHTML = "";
+  }
+
+  clearMessages();
+  /* remove contents of titleList */
+  /* Done */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  /* find all the articles and save them to variable: articles */
+  /* Done */
+
+  let html = "";
+
+  for (let article of articles) {
+    const articleId = article.getAttribute("id");
+    /* get the article id */
+    /* Done */
+
+    const articleTitleElement = article.querySelector(optTitleSelector);
+    /* find the title element */
+    /* ... */
+
+    const articleTitle = articleTitleElement.innerHTML;
+    /* get the title from the title element */
+    /* Done */
+
+    const linkHTML =
+      '<li><a href="#' +
+      articleId +
+      '"><span>' +
+      articleTitle +
+      "</span></a></li>";
+    /* create HTML of the link */
+    /* Done */
+
+    html = html + linkHTML;
+    /* insert link into html variable */
+  }
+
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll(".titles a");
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
+  }
 }
+
+generateTitleLinks();
